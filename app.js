05924642195a71c0e44810da9fa1d81607bd8c86
Pranit Shrivastava <pranitshrivastava010@gmail.com>
2025-05-13 14:09:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -63,5 +63,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 
 const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 
 
